1d4ce8e973052bd69a60c4b1954b40be8dcc795b
Salah Mahmoud <SalahMahmoud202@gmail.com>
2021-02-20 23:18:01 +0200

    changed the Std_Types file to autosar compatable by adding 1-Std_Return types 2- modified platform types 3-added SW and Autosar version

--- Common/Std_Types.h
+++ Common/Std_Types.h
@@ -6,11 +6,44 @@
  *         File:  <Std_Types>
  *       Module:  Common
  *
- *  Description:  <This file defines the std type that are used accross the project in common folder >     
- *  
+ *  Description:  This file defines the std type that are used accross the project in common folder     
+ *	Author: Salah Mahmoud 
+ *
  *********************************************************************************************************************/
 #ifndef STD_TYPES_H
 #define STD_TYPES_H
+
+
+/**********************************************************************************************************************
+ *  SOFTWARE AND AUTOSAR VERSIONS
+ *********************************************************************************************************************/
+
+
+/* Defining the company ID in the AUTOSAR */
+/* I am using a dummy ID for now */
+
+#define COMPILER_VENDOR_ID					(2007U)
+
+
+/* Defining the Module software verison by Major.Minor.Patch format */
+
+#define STD_TYPES_SW_MAJOR_VERSION			(1U)
+#define STD_TYPES_SW_MINOR_VERSION			(0U)
+#define STD_TYPES_SW_PATCH_VERSION			(0U)
+
+/* Defining the autosar version ued for the module */
+
+#define STD_TYPES_AR_RELEASE_MAJOR_VERSION		(U)
+#define STD_TYPES_AR_RELEASE_MINOR_VERSION		(U)
+#define STD_TYPES_AR_RELEASE_PATCH_VERSION		(U)
+
+
+
+
+
+
+
+
 
 /**********************************************************************************************************************
  * INCLUDES
@@ -19,16 +52,16 @@
 /**********************************************************************************************************************
  *  GLOBAL DATA TYPES AND STRUCTURES
  *********************************************************************************************************************/
-typedef char int8;					//define signed char as int8 
+typedef char sint8;					//define signed char as int8 
 typedef unsigned char uint8;		//define unsigned char as uint8 
-typedef short int16;				//define signed short as int16 
+typedef short sint16;				//define signed short as int16 
 typedef unsigned short uint16;		//define unsigned short as uint16 
-typedef long int32;					//define signed long as int32 
+typedef long sint32;					//define signed long as int32 
 typedef unsigned long uint32;		//define unsigned long as uint32 
-typedef long long int64;			//define signed loong long as int64 
+typedef long long sint64;			//define signed loong long as int64 
 typedef unsigned long long uint64;	//define unsigned long long as uint64 
-typedef float f32;					//define float as f32 
-typedef double f64;		
+typedef float float32;					//define float as f32 
+typedef double float64;		
 
 typedef uint8 Std_returnType;
 
@@ -42,15 +75,17 @@ typedef struct
     uint8 sw_patch_version;
 } Std_VersionInfoType;
 
-#define STD_HIGH        0x01    /*physical state 5V or 3.3V */
-#define STD_LOW         0x00    /*physical state 0V */
+#define STD_HIGH        (0x01U)    /*physical state 5V or 3.3V */
+#define STD_LOW         (0x00U)    /*physical state 0V */
 
-#define STD_ACTIVE      0x01    /* Logical state active */
-#define STD_IDLE        0x00    /* Logical state idle */ 
+#define STD_ACTIVE      (0x01U)    /* Logical state active */
+#define STD_IDLE        (0x00U)    /* Logical state idle */ 
 
-#define STD_ON          0x01    /* Logical state ON */
-#define STD_OFF         0x00    /* Logical state OFF */
+#define STD_ON          (0x01U)    /* Logical state ON */
+#define STD_OFF         (0x00U)    /* Logical state OFF */
 
+#define E_OK			((Std_returnType)0x00U))
+#define E_NOT_OK		((Std_returnType)0x01U))
 
 #endif  /* STD_TYPES_H */
 
